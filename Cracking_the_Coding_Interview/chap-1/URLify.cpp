cab22b5f57ba7b17f80e9846f6e809657777ece6
Tapan Kumar Mishra <tapankumar.mishra@intel.com>
2020-04-26 14:50:03 +0300

    - Adding 1.4.palindrome_permutaion.cpp - Updating indentation by replacing tab with 4 ' 's

--- Cracking_the_Coding_Interview/chap-1/URLify.cpp
+++ Cracking_the_Coding_Interview/chap-1/URLify.cpp
@@ -3,43 +3,43 @@
 #include <cstring>
 using namespace std;
 class urlify {
-	char *baseptr = NULL;
-	int len = 0;
+    char *baseptr = NULL;
+    int len = 0;
 public:
-	urlify(char *str, int size);
-	void show() {
-		if(baseptr)
-			printf("%s\n", baseptr);
-	}
+    urlify(char *str, int size);
+    void show() {
+        if(baseptr)
+            printf("%s\n", baseptr);
+    }
 };
 
 urlify::urlify(char *str, int size) {
-	baseptr = str;
-	len = size;
-	char *s = new char[size];
-	char *cp = s;
-	if(!s) {
-		cout << "unable to allocate memory " << size << endl;
-		return;
-	}
-	for(int i=0,j=0; i< size && j < size; i++, j++) {
-		if(baseptr[i] == ' ') {
-			*cp++ = '%';
-			*cp++ = '2';
-			*cp++ = '0';
-			j += 2;
-		} else {
-			*cp++ = baseptr[i];
-		}
-	}
-	memcpy(baseptr, s, size);
-	delete [] s;
+    baseptr = str;
+    len = size;
+    char *s = new char[size];
+    char *cp = s;
+    if(!s) {
+        cout << "unable to allocate memory " << size << endl;
+        return;
+    }
+    for(int i=0,j=0; i< size && j < size; i++, j++) {
+        if(baseptr[i] == ' ') {
+            *cp++ = '%';
+            *cp++ = '2';
+            *cp++ = '0';
+            j += 2;
+        } else {
+            *cp++ = baseptr[i];
+        }
+    }
+    memcpy(baseptr, s, size);
+    delete [] s;
 }
 
 int main()
 {
-	char mystring[15] = "hello world\n";
-	class urlify u = urlify(mystring, sizeof(mystring));
-	u.show();
-	return 0;
+    char mystring[15] = "hello world\n";
+    class urlify u = urlify(mystring, sizeof(mystring));
+    u.show();
+    return 0;
 }
--- Cracking_the_Coding_Interview/chap-1/unique_letter_in_str.cpp
+++ Cracking_the_Coding_Interview/chap-1/unique_letter_in_str.cpp
@@ -7,84 +7,84 @@
 using namespace std;
 
 struct mylist {
-	struct mylist *next;
-	char c;
+    struct mylist *next;
+    char c;
 };
 
 struct hashentry {
-	struct mylist *node;
-	int count;
-	char c;
+    struct mylist *node;
+    int count;
+    char c;
 };
 
 class mystring {
-	string basestr;
-	struct hashentry *hashtable[256]={NULL};
-	vector<char> unique;
-	int ucnt;
+    string basestr;
+    struct hashentry *hashtable[256]={NULL};
+    vector<char> unique;
+    int ucnt;
 public:
-	mystring() {
-		ucnt = 0;
-	}
-	int addtohashtable(char c);
-	mystring(const string & str) {
-		basestr = str;
-		printf("initializing object, unquevect size = %ld\n", unique.size());
-		for (auto & c: basestr) {
-			int num = addtohashtable(c);
-			if(num == 1) {
-				ucnt++;
-			}
-		}
-		printf("number of unique characters = %d\n", ucnt);
-	}
-	void show(void);
+    mystring() {
+        ucnt = 0;
+    }
+    int addtohashtable(char c);
+    mystring(const string & str) {
+        basestr = str;
+        printf("initializing object, unquevect size = %ld\n", unique.size());
+        for (auto & c: basestr) {
+            int num = addtohashtable(c);
+            if(num == 1) {
+                ucnt++;
+            }
+        }
+        printf("number of unique characters = %d\n", ucnt);
+    }
+    void show(void);
 };
 
 int mystring::addtohashtable(char c) {
-	int key = getkey(c);
-	int idx = key % (sizeof(hashtable)/sizeof(hashtable[0]));
-	struct hashentry *hentry = (hashtable[idx]);
-	struct mylist *node = new (struct mylist);
-	if(!node) {
-		cout << "Unable to allocate memory" << endl;
-		return 0;
-	}
-	memset(node, 0, sizeof(struct mylist));
-	node->c = c;
-	if(hentry) {
-		struct mylist *nhead = (hashtable[idx]->node);
-		struct mylist *n = nhead;
-		for(;n->next; n = n->next);
-		n->next = node;
-	} else {
-		hentry = new (struct hashentry);
-		if(!hentry) {
-			cout << "Unable to allocate memory" << endl;
-			return 0;
-		}
+    int key = getkey(c);
+    int idx = key % (sizeof(hashtable)/sizeof(hashtable[0]));
+    struct hashentry *hentry = (hashtable[idx]);
+    struct mylist *node = new (struct mylist);
+    if(!node) {
+        cout << "Unable to allocate memory" << endl;
+        return 0;
+    }
+    memset(node, 0, sizeof(struct mylist));
+    node->c = c;
+    if(hentry) {
+        struct mylist *nhead = (hashtable[idx]->node);
+        struct mylist *n = nhead;
+        for(;n->next; n = n->next);
+        n->next = node;
+    } else {
+        hentry = new (struct hashentry);
+        if(!hentry) {
+            cout << "Unable to allocate memory" << endl;
+            return 0;
+        }
 
-		memset(hentry, 0, sizeof(struct hashentry));
-		hentry->node = node;
-		hentry->c = c;
-		hashtable[idx] = hentry;
-	}
-	hentry->count++;
-	return hentry->count;
+        memset(hentry, 0, sizeof(struct hashentry));
+        hentry->node = node;
+        hentry->c = c;
+        hashtable[idx] = hentry;
+    }
+    hentry->count++;
+    return hentry->count;
 }
 void mystring::show(void) {
-	for(auto & he: hashtable) {
-		if(he) {
-			printf("%-10c(ascii:%d): %d\n", he->c, he->c, he->count);
-		}
-	}
+    for(auto & he: hashtable) {
+        if(he) {
+            printf("%-10c(ascii:%d): %d\n", he->c, he->c, he->count);
+        }
+    }
 }
 
 
 int main()
 {
-	mystring s = mystring("hello world\n");
-	s.show();
-	return 0;
+    mystring s = mystring("hello world\n");
+    s.show();
+    return 0;
 }
 
